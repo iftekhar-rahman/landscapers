da6f38a9f682fca8aef663b8c6f18a44f90100bf
iftekhar-rahman <iftekharmail1@gmail.com>
2019-06-25 22:13:18 -0700

    fial update

--- js/main.js
+++ js/main.js
@@ -105,10 +105,22 @@ jQuery(document).ready(function($){
         $(".search-box").removeClass("active");
     }) ;
 
-    
-    // $(".scroll-next-section").on("click", function(){
-    //     $("#sjhadkl").scrollTop(0);
-    // })
+
+    $(".scroll-next-section").on('click', function(event) {
+        if (this.hash !== "") {
+            
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+            scrollTop: $(hash).offset().top
+            }, 800, function(){
+        
+            window.location.hash = hash;
+            });
+        } // End if
+    });
     
 
 });
